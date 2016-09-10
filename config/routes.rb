a83190ac299714e357478092a2205c3ef7b0b338
vogdev <vogdev@gmail.com>
2016-09-10 08:32:33 +0100

    Add Users_controoler and user show/edit pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 
 Rails.application.routes.draw do
+  get 'users/show'
+
   devise_for :users
+	resources :users, only: [:show, :edit, :update]
 	resources :posts, only: [:new, :create]
   root 'homes#show'
 end
