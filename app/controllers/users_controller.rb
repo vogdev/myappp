3a6b7e0a75f1bfff631545cd1fcbf24672e5fb48
vogdev <vogdev@gmail.com>
2016-09-10 09:08:00 +0100

    Add Avatarupleader to user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,6 @@ end
         @user = current_user
   end
    def user_params
-       params.require(:user).permit(:username)
+       params.require(:user).permit(:username, :avatar)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ActiveRecord::Base
-	has_many :posts, dependent: :destroy
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
- validates  :username, presence: true
+has_many :posts, dependent: :destroy
+devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+validates  :username, presence: true
+mount_uploader :avatar, AvatarUploader
 end
